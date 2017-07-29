22e5cf3cfc5e962c88f83cc1f06208852142509a
Diego Saouda <dsaouda@gmail.com>
2017-07-28 22:05:02 -0300

    ajustado o flags

--- esb-login-api/main.go
+++ esb-login-api/main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"github.com/dsaouda/fiap-esb/esb-login-api/server"
+	"flag"
+	"fmt"
 )
 
 func main() {
+	port := flag.Int("port", 9002, "porta")
+	host := flag.String("host", "localhost", "host")
+	flag.Parse()
+	listen := fmt.Sprintf("%s:%d", *host, *port)
+
 	s := server.Setup()
-	s.Run("localhost:9002")
+	s.Run(listen)
 }
